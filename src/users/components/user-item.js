9e39969d87f4bd1d41e3b7b3912356f747fb97b8
DenisPepper <thech@yandex.ru>
2023-12-06 15:00:32 +0300

    Добавит карточку пользователя

---
+++ src/users/components/user-item.js
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom';
+import Avatar from '../../shared/components/ui-elements/avatar';
+import './user-item.css';
+
+export const UserItem = (props) => {
+  const { id, image, name, placeCount } = props;
+  return (
+    <li className='user-item'>
+      <div className='user-item__content'>
+        <Link to={`/${id}/places`}>
+          <div className='user-item__image'>
+            <Avatar image={image} alt={name} />
+          </div>
+          <div className='user-item__info'>
+            <h2>{name}</h2>
+            <h3>
+              {placeCount} {placeCount === 1 ? 'place' : 'places'}
+            </h3>
+          </div>
+        </Link>
+      </div>
+    </li>
+  );
+};

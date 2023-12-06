6874660bd9540a310c84bfa0b787ae6577ddc00f
DenisPepper <thech@yandex.ru>
2023-12-06 21:37:20 +0300

    Переместит сайдбар в портал

--- src/shared/components/navigation/side-drawer.js
+++ src/shared/components/navigation/side-drawer.js
@@ -1,6 +1,8 @@
 import './side-drawer.css';
+import { createPortal } from 'react-dom';
 
 export const SideDrawer = (props) => {
   const { children } = props;
-  return <aside className='side-drawer'>{children}</aside>;
+  const content = <aside className='side-drawer'>{children}</aside>;
+  return createPortal(content, document.getElementById('drawer-hook'));
 };

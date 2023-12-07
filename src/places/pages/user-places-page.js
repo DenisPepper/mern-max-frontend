d996ad4508644b39388871ceacc8c8841371a150
DenisPepper <thech@yandex.ru>
2023-12-07 21:24:44 +0300

    Добавит моковые данные мест

--- src/places/pages/user-places-page.js
+++ src/places/pages/user-places-page.js
@@ -1,3 +1,32 @@
+import { PlaceList } from '../components/place-list';
+
+const items = [
+  {
+    id: 'p1',
+    imageUrl: '/images/places/place-1.jpg',
+    name: 'City',
+    description: 'Very big city',
+    address: 'Adress',
+    creator: 'u1',
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
+    },
+  },
+  {
+    id: 'p2',
+    imageUrl: '/images/places/place-1.jpg',
+    name: 'City',
+    description: 'Very big city',
+    address: 'Adress',
+    creator: 'u2',
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
+    },
+  },
+];
+
 export const UserPlacesPage = () => {
-    
-}
+  return <PlaceList items={items} />;
+};
